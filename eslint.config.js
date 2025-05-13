9236c0131de5f3690b8f331fa2b0fc4b04007d49
Crisops <crialeperez1835@gmail.com>
2025-05-12 20:33:22 -0500

    Actualizar configuración de ESLint: se añade la configuración recomendada de eslint-plugin-astro y se ajusta la configuración de neostandard para deshabilitar estilos.

--- eslint.config.js
+++ eslint.config.js
@@ -5,16 +5,10 @@ import neostandard from 'neostandard'
 export default defineConfig([
   // add more generic rule sets here, such as:
   // js.configs.recommended,
+  ...eslintPluginAstro.configs.recommended,
   ...neostandard({
     files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
     ts: true,
+    noStyle: true,
   }),
-  {
-    files: ['**/*.astro'],
-    rules: {
-      ...eslintPluginAstro.rules,
-      // override/add rules settings here, such as:
-      // "astro/no-set-html-directive": "error"
-    },
-  },
 ])
