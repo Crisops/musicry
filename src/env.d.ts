9176ea0db7c184415d23c030c393202e6b367284
Crisops <crialeperez1835@gmail.com>
2025-05-22 17:07:48 -0500

    Agregar la variable de entorno EMAIL_ADMIN

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,6 +2,7 @@ interface ImportMetaEnv {
   readonly SUPABASE_URL: string
   readonly SUPABASE_ANON_KEY: string
   readonly SITE_URL: string
+  readonly EMAIL_ADMIN: string
 }
 
 interface ImportMeta {
