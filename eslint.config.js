7b04d92180d63d06cef2ac5a229d745e814d0efe
Crisops <crialeperez1835@gmail.com>
2025-05-28 15:14:14 -0500

    Agregar configuración específica para archivos .astro en eslint.config.js: se desactivan varias reglas de React para mejorar la compatibilidad y flexibilidad en el uso de componentes Astro.

--- eslint.config.js
+++ eslint.config.js
@@ -3,12 +3,23 @@ import eslintPluginAstro from 'eslint-plugin-astro'
 import neostandard from 'neostandard'
 
 export default defineConfig([
-  // add more generic rule sets here, such as:
-  // js.configs.recommended,
   ...eslintPluginAstro.configs.recommended,
   ...neostandard({
     files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
     ts: true,
     noStyle: true,
   }),
+  // Configuración específica para archivos .astro
+  {
+    files: ['**/*.astro'],
+    rules: {
+      'react/self-closing-comp': 'off',
+      'react/jsx-key': 'off',
+      'react/jsx-no-undef': 'off',
+      'react/jsx-uses-react': 'off',
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
+      'react/display-name': 'off',
+    },
+  },
 ])
