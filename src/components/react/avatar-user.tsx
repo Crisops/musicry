aec9d2fcf5ef12a3e6849dcc3afb8d1ef68b1091
Crisops <crialeperez1835@gmail.com>
2025-05-28 15:19:34 -0500

    Eliminar referencia de imagen en el componente AvatarUser: se simplifica el componente al eliminar la propiedad imageRef, mejorando la claridad y reduciendo la complejidad del código.

--- src/components/react/avatar-user.tsx
+++ src/components/react/avatar-user.tsx
@@ -2,14 +2,12 @@ import type { User } from '@supabase/supabase-js'
 
 interface Props {
   user: User
-  imageRef: React.RefObject<HTMLImageElement>
 }
 
-const AvatarUser = ({ user, imageRef }: Props) => {
+const AvatarUser = ({ user }: Props) => {
   return (
     <div className="absolute size-6 h-full w-full rounded-full">
       <img
-        ref={imageRef}
         className="absolute h-full w-full object-cover"
         src={user.user_metadata.avatar_url}
         alt={user.user_metadata.name}
