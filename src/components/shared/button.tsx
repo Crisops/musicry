dbf85f4a3c4e1fa8edbbff0eea04dfd9d6c52494
Crisops <crialeperez1835@gmail.com>
2025-05-31 18:35:59 -0500

    Actualizar el componente Button: se añade la función 'cn' para gestionar clases dinámicas, mejorando la presentación y la interactividad del botón en la interfaz de usuario.

--- src/components/shared/button.tsx
+++ src/components/shared/button.tsx
@@ -1,3 +1,4 @@
+import { cn } from '@/lib/utils'
 import {
   type ButtonProps as ButtonPropsHero,
   Button as ButtonHero,
@@ -8,7 +9,17 @@ export interface ButtonProps extends ButtonPropsHero {
 }
 
 const Button = ({ children, ...props }: ButtonProps) => {
-  return <ButtonHero {...props}>{children}</ButtonHero>
+  return (
+    <ButtonHero
+      className={cn(
+        'data-[hover=true]:opacity-disabled data-[hover=true]:hover:opacity-100',
+        props.className,
+      )}
+      {...props}
+    >
+      {children}
+    </ButtonHero>
+  )
 }
 
 export default Button
