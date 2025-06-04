c2d5d8a7699dd127ea52e554829f7c9b7865f0a4
Crisops <crialeperez1835@gmail.com>
2025-06-04 12:58:43 -0500

    Eliminar el componente Link: se ha eliminado el archivo link.tsx

--- src/components/react/link.tsx
+++
@@ -1,7 +0,0 @@
-import { Link as LinkHero, type LinkProps } from '@heroui/link'
-
-const Link = ({ children, ...props }: LinkProps) => {
-  return <LinkHero {...props}>{children}</LinkHero>
-}
-
-export default Link
