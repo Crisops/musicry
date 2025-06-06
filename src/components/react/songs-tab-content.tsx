a475755aee8a456060eee5fc77543d2b42d94b6b
Crisops <crialeperez1835@gmail.com>
2025-06-06 14:38:57 -0500

    Agregar nuevos componentes AlbumsTabContent y SongsTabContent: se crean los archivos albums-tab-content.tsx y songs-tab-content.tsx que implementan la visualización de listas de pistas para álbumes y canciones, mejorando la navegación y la experiencia del usuario en la aplicación.

---
+++ src/components/react/songs-tab-content.tsx
@@ -0,0 +1,34 @@
+import TrackList from '@/components/react/track-list'
+import { trackListAlbumColumnsDashboard, tracks } from '@/types/track'
+import TopTableDashboard from '@/components/react/top-table-dashboard'
+
+const SongsTabContent = ({ tap }: { tap: 'songs' | 'albums' }) => {
+  return (
+    <div className="grid-row-1 grid h-full">
+      <div className="scrollbar scrollbar-w-1 scrollbar-thumb-rounded-full scrollbar-thumb-rich-dark-jungle overflow-y-auto">
+        <TrackList
+          topContent={<TopTableDashboard tap={tap} />}
+          selectionMode="single"
+          classNames={{
+            base: ['flex flex-col h-0'],
+            thead: ['border-b border-gray-dim'],
+            th: ['bg-transparent', 'text-sealsalt', 'border-divider'],
+            td: [
+              'first:before:rounded-none last:before:rounded-none',
+              'before:bg-rich-black-light',
+              'data-[selected=true]:text-platinum',
+              'group-aria-[selected=false]/tr:group-data-[hover=true]/tr:before:bg-rich-black-light/60',
+              'group-aria-[selected=true]/tr:group-data-[hover=true]/tr:before:bg-rich-black-light',
+            ],
+            tr: ['text-gray-dim'],
+          }}
+          removeWrapper
+          rows={tracks}
+          columns={trackListAlbumColumnsDashboard}
+        />
+      </div>
+    </div>
+  )
+}
+
+export default SongsTabContent
