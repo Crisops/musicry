ac7c952f1a38b28b842eae7ae2a1cdc0c7b3dbdd
Crisops <crialeperez1835@gmail.com>
2025-06-13 14:58:57 -0500

    Agregar nuevo componente Input: se crea el archivo input.tsx que implementa un campo de entrada personalizado utilizando InputHero, mejorando la interfaz de usuario y la experiencia de entrada de datos.

---
+++ src/components/react/input.tsx
@@ -0,0 +1,23 @@
+import { Input as InputHero, type InputProps } from '@heroui/react'
+
+const Input = (props: InputProps) => {
+  return (
+    <InputHero
+      {...props}
+      size="sm"
+      radius="sm"
+      className="max-w-"
+      classNames={{
+        inputWrapper: [
+          'bg-night group-data-[focus=true]:bg-night data-[hover=true]:bg-night/90 group-data-[invalid=true]:!bg-red-500/10 group-data-[invalid=true]:data-[hover=true]:!bg-red-500/15',
+        ],
+        label: ['text-gray-dim'],
+        input: ['group-data-[has-value=true]:text-sealsalt'],
+        description: ['text-gray-dim'],
+        helperWrapper: ['max-w-md'],
+      }}
+    />
+  )
+}
+
+export default Input
