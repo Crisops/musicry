b0181c5739ed7481fafd45b2497e6d260f9ca509
Crisops <crialeperez1835@gmail.com>
2025-06-19 14:55:26 -0500

    Actualizar la interfaz ImportMetaEnv
    -Renombrar las variables SUPABASE_URL y SUPABASE_ANON_KEY a PUBLIC_SUPABASE_URL y PUBLIC_SUPABASE_ANON_KEY.
    
    - Además, se añade un tipo User que define la estructura del usuario y se extiende la interfaz Locals en el namespace App para incluir el usuario.

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,10 +1,24 @@
 interface ImportMetaEnv {
-  readonly SUPABASE_URL: string
-  readonly SUPABASE_ANON_KEY: string
+  readonly PUBLIC_SUPABASE_URL: string
+  readonly PUBLIC_SUPABASE_ANON_KEY: string
   readonly SITE_URL: string
   readonly EMAIL_ADMIN: string
 }
 
 interface ImportMeta {
   readonly env: ImportMetaEnv
+}
+
+type User = {
+  id: string
+  full_name?: string
+  email?: string
+  avatar_url?: string
+  isAdmin: boolean
+} | null
+
+declare namespace App {
+  interface Locals {
+    user: User
+  }
 }
