c3c9a90c227c6a81d700b04431fa523c32b34989
Crisops <crialeperez1835@gmail.com>
2025-06-19 14:57:52 -0500

    Actualizar middleware para la autenticación de usuarios:
    - Se reemplaza la lógica de manejo de sesiones de Supabase, utilizando el nuevo cliente creado en `supabase/server.ts`.
    
    - Se simplifica la verificación de rutas protegidas y se gestiona la información del usuario en `locals`.
    
    - Se eliminan las referencias a tokens en cookies, mejorando la seguridad y la claridad del código.

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,9 +1,8 @@
 import { defineMiddleware } from 'astro:middleware'
-import { supabase } from '@/lib/supabase'
 import picomatch from 'picomatch'
+import { createClient } from '@/lib/supabase/server'
 
 const protectedRoutesPages = ['/admin/dashboard(|/)']
-const redirectRoutesPages = ['/signin(|/)', '/register(|/)']
 const proptectedAPIRoutes = [
   '/api/users(|/)',
   '/api/auth(|/)',
@@ -12,66 +11,42 @@ const proptectedAPIRoutes = [
   '/api/albums(|/)',
   '/api/stats(|/)',
 ]
+const publicRoutes = ['/', '/api/auth/(signin|signup|callback)']
 
 export const onRequest = defineMiddleware(
-  async ({ url, cookies, redirect }, next) => {
-    if (picomatch.isMatch(url.pathname, protectedRoutesPages)) {
-      const accessToken = cookies.get('sb-access-token')
-      const refreshToken = cookies.get('sb-refresh-token')
+  async ({ url, cookies, redirect, request, locals }, next) => {
+    const supabase = await createClient(request, cookies)
+    const {
+      data: { user },
+    } = await supabase.auth.getUser()
 
-      if (!accessToken || !refreshToken) {
-        return redirect('/')
+    if (user) {
+      locals.user = {
+        id: user.id,
+        email: user.email,
+        full_name: user.user_metadata.full_name,
+        avatar_url: user.user_metadata.avatar_url,
+        isAdmin: user.email === import.meta.env.EMAIL_ADMIN,
       }
-
-      const { data, error } = await supabase.auth.setSession({
-        access_token: accessToken.value,
-        refresh_token: refreshToken.value,
-      })
-
-      if (error) {
-        cookies.delete('sb-access-token', { path: '/' })
-        cookies.delete('sb-refresh-token', { path: '/' })
-        return redirect('/')
-      }
-
-      cookies.set('sb-access-token', data?.session?.access_token!, {
-        sameSite: 'strict',
-        path: '/',
-        secure: true,
-      })
-
-      cookies.set('sb-refresh-token', data?.session?.refresh_token!, {
-        sameSite: 'strict',
-        path: '/',
-        secure: true,
-      })
     }
 
-    if (picomatch.isMatch(url.pathname, redirectRoutesPages)) {
-      const accessToken = cookies.get('sb-access-token')
-      const refreshToken = cookies.get('sb-refresh-token')
+    if (picomatch.isMatch(url.pathname, publicRoutes)) {
+      return next()
+    }
 
-      if (accessToken && refreshToken) {
+    if (!user) {
+      return redirect('/')
+    }
+
+    if (picomatch.isMatch(url.pathname, protectedRoutesPages)) {
+      if (locals.user?.email !== import.meta.env.EMAIL_ADMIN) {
         return redirect('/')
       }
     }
 
     if (picomatch.isMatch(url.pathname, proptectedAPIRoutes)) {
-      const accessToken = cookies.get('sb-access-token')
-      const refreshToken = cookies.get('sb-refresh-token')
-
-      // Check for tokens
-      if (!accessToken || !refreshToken) {
-        return new Response(JSON.stringify({ error: 'Unauthorized' }), {
-          status: 401,
-        })
-      }
-
-      const { error } = await supabase.auth.setSession({
-        access_token: accessToken.value,
-        refresh_token: refreshToken.value,
-      })
-
+      const supabase = await createClient(request, cookies)
+      const { error } = await supabase.auth.getSession()
       if (error) {
         return new Response(JSON.stringify({ error: 'Unauthorized' }), {
           status: 401,
