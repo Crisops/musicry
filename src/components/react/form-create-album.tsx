b7b0955e9cf321857c5591ff0065cbc036e26403
Crisops <crialeperez1835@gmail.com>
2025-06-24 16:04:52 -0500

    Actualizar el componente `FormCreateAlbum` para incluir la gestión de la propiedad `user` y mejorar la lógica de creación de álbumes:
    - Se añade la validación para asegurar que `user`, `imageUrl` y `releaseYear` estén presentes antes de proceder con la creación del álbum.
    
    - Se implementa el uso de `Controller` de `react-hook-form` para manejar el campo `releaseYear` con validaciones específicas.
    
    - Se mejora el manejo de errores y se implementan notificaciones de éxito y error al crear un álbum.

--- src/components/react/form-create-album.tsx
+++ src/components/react/form-create-album.tsx
@@ -1,25 +1,76 @@
+import { getLocalTimeZone, today } from '@internationalized/date'
+import { v4 as uuidv4 } from 'uuid'
+import { Controller } from 'react-hook-form'
+import { DateInput } from '@heroui/date-input'
 import { useForm } from '@/hooks/use-form'
-import type { DateValue } from '@internationalized/date'
+import { useModal } from '@/hooks/use-modal'
+import type { TablesInsert } from '@/types/database.types'
 import type { AlbumFormData } from '@/utils/validate-rules-form'
+import { albumValidationRules } from '@/utils/validate-rules-form'
+import { uploadFile } from '@/utils/upload-files'
+import { Toast } from '@/utils/toast'
 import { initialFormAlbum } from '@/config/fields-form'
 import Button from '@/components/shared/button'
-import Input from '@/components/react/input'
 import { FileUpload } from '@/components/ui/file-upload'
-import { DateInput } from '@heroui/date-input'
-import { useState } from 'react'
+import Input from '@/components/react/input'
 
 interface FormCreateAlbumProps {
+  user: User
   onCancel: (open: boolean) => void
 }
 
-const FormCreateAlbum = ({ onCancel }: FormCreateAlbumProps) => {
-  const { registerField, handleSubmit, errors, isSubmitting } =
+const FormCreateAlbum = ({ user, onCancel }: FormCreateAlbumProps) => {
+  const { registerField, handleSubmit, errors, isSubmitting, control } =
     useForm<AlbumFormData>({ initialForm: initialFormAlbum })
+  const { onOpenChange: closeModal } = useModal()
 
   const { onChange, ref, ...props } = registerField('imageUrl')
 
   const onSubmit = handleSubmit(async (data) => {
-    console.log(data)
+    const { imageUrl, releaseYear, ...formData } = data
+
+    if (!user || !imageUrl || !releaseYear) return
+
+    try {
+      const idAlbum = uuidv4()
+      const folder = `${user.id}/${idAlbum}`
+      const { url, error } = await uploadFile({
+        bucket: 'albums',
+        file: imageUrl[0],
+        folder,
+      })
+      if (error) throw new Error(error)
+
+      const newAlbum: TablesInsert<'albums'> = {
+        id: idAlbum,
+        ...formData,
+        imageUrl: url,
+        releaseYear: releaseYear.toDate(getLocalTimeZone()).toISOString(),
+      }
+
+      const response = await fetch('/api/admin/albums', {
+        method: 'POST',
+        body: JSON.stringify(newAlbum),
+      })
+      if (!response.ok) throw new Error('Fallo al crear el albúm')
+      const { status, message } = await response.json()
+      if (status !== 200) throw new Error(message)
+
+      Toast().success({
+        title: 'Albúm creado correctamente',
+        description: `El albúm ${newAlbum.title} ha sido creado correctamente`,
+      })
+      closeModal(false)
+    } catch (error) {
+      const message =
+        error instanceof Error
+          ? error.message
+          : 'No pudimos crear tu albúm. Intenta nuevamente.'
+      Toast().error({
+        title: '¡Ups! Algo salió mal',
+        description: message,
+      })
+    }
   })
 
   return (
@@ -49,23 +100,33 @@ const FormCreateAlbum = ({ onCancel }: FormCreateAlbumProps) => {
           errorMessage={errors.artist?.message}
           description="Escribe el nombre del artista o artistas. Si hay más de uno, sepáralos con comas (Ej: Karol G, Feid, Bizarrap, etc.)"
         />
-        <DateInput
-          isInvalid={!!errors.releaseYear}
-          errorMessage={errors.releaseYear?.message}
-          classNames={{
-            inputWrapper: [
-              'bg-night focus-within:hover:bg-night hover:bg-night/90 group-data-[invalid=true]:!bg-red-500/10 group-data-[invalid=true]:data-[hover=true]:!bg-red-500/15',
-            ],
-            label: ['text-gray-dim'],
-            input: ['text-sealsalt'],
-            helperWrapper: ['max-w-md'],
-            segment: [
-              'focus:bg-gray-jet/20  data-[editable=true]:text-gray-dim data-[editable=true]:focus:text-gray-davy',
-            ],
+        <Controller
+          control={control}
+          name="releaseYear"
+          rules={{
+            validate: albumValidationRules.releaseYear.validate,
           }}
-          label={'Año de lanzamiento'}
+          render={({ field: { value, ...field }, fieldState }) => (
+            <DateInput
+              {...field}
+              maxValue={today(getLocalTimeZone())}
+              classNames={{
+                inputWrapper: [
+                  'bg-night focus-within:hover:bg-night hover:bg-night/90 group-data-[invalid=true]:!bg-red-500/10 group-data-[invalid=true]:data-[hover=true]:!bg-red-500/15',
+                ],
+                label: ['text-gray-dim'],
+                input: ['text-sealsalt'],
+                helperWrapper: ['max-w-md'],
+                segment: [
+                  'focus:bg-gray-jet/20 data-[editable=true]:text-sealsalt data-[editable=true]:focus:text-gray-davy data-[editable=true]:data-[placeholder=true]:text-gray-dim data-[invalid=true]:data-[editable=true]:data-[placeholder=true]:text-danger',
+                ],
+              }}
+              label={'Año de lanzamiento'}
+              isInvalid={!!fieldState.error}
+              errorMessage={fieldState.error?.message}
+            />
+          )}
         />
-
         <div className="flex justify-end gap-2">
           <Button
             radius="sm"
