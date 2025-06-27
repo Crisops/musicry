adab6bd7106a414e5a446467a696e5a590eb1b9c
Crisops <crialeperez1835@gmail.com>
2025-06-27 12:46:14 -0500

    Refactorizar tipos y constantes en `track.ts` para mejorar la estructura de datos:
    - Se renombraron y ajustaron tipos de columnas y filas para álbumes y canciones.
    
    - Se actualizó la propiedad `created_at` a `release_year` en varios lugares.
    
    - Se reorganizaron las constantes de columnas para una mejor claridad y uso en la interfaz.

--- src/types/track.ts
+++ src/types/track.ts
@@ -1,19 +1,19 @@
-import type { Tables } from './database.types'
+import type { Tables } from '@/types/database.types'
 
-export type TrackListAlbumColumns = {
+export interface BaseTrackRow {
+  id: Tables<'songs'>['id'] | Tables<'albums'>['id']
+  title: Tables<'songs'>['title'] | Tables<'albums'>['title']
+  artist: Tables<'songs'>['artist'] | Tables<'albums'>['artist']
+  imageUrl: Tables<'songs'>['imageUrl'] | Tables<'albums'>['imageUrl']
+  release_year: Tables<'songs'>['created_at'] | Tables<'albums'>['releaseYear']
+}
+
+export type Column = {
   key: string
   label: string
 }
 
-export type TrackListAlbumRows = {
-  id: Tables<'songs'>['id']
-  title: Tables<'songs'>['title']
-  artist: Tables<'songs'>['artist']
-  duration: Tables<'songs'>['duration']
-  created_at: Tables<'songs'>['created_at']
-}
-
-export const trackListAlbumColumns: TrackListAlbumColumns[] = [
+export const trackAlbumPageColumns: Column[] = [
   {
     key: 'id',
     label: '#',
@@ -23,7 +23,7 @@ export const trackListAlbumColumns: TrackListAlbumColumns[] = [
     label: 'Titulo',
   },
   {
-    key: 'created_at',
+    key: 'release_year',
     label: 'Fecha de publicación',
   },
   {
@@ -32,7 +32,7 @@ export const trackListAlbumColumns: TrackListAlbumColumns[] = [
   },
 ]
 
-export const trackListAlbumColumnsDashboard: TrackListAlbumColumns[] = [
+export const trackAlbumPanelColumns: Column[] = [
   {
     key: 'title',
     label: 'Titulo',
@@ -42,7 +42,30 @@ export const trackListAlbumColumnsDashboard: TrackListAlbumColumns[] = [
     label: 'Artistas',
   },
   {
-    key: 'created_at',
+    key: 'release_year',
+    label: 'Fecha de publicación',
+  },
+  {
+    key: 'songs',
+    label: 'Canciones',
+  },
+  {
+    key: 'action',
+    label: 'Acción',
+  },
+]
+
+export const trackSongsPanelColumns: Column[] = [
+  {
+    key: 'title',
+    label: 'Titulo',
+  },
+  {
+    key: 'artist',
+    label: 'Artistas',
+  },
+  {
+    key: 'release_year',
     label: 'Fecha de publicación',
   },
   {
@@ -51,102 +74,114 @@ export const trackListAlbumColumnsDashboard: TrackListAlbumColumns[] = [
   },
 ]
 
-export type Track = {
-  id: Tables<'songs'>['id']
-  image: Tables<'songs'>['imageUrl']
-  title: Tables<'songs'>['title']
-  artist: Tables<'songs'>['artist']
-  created_at: Tables<'songs'>['created_at']
+export type TrackSongsListPanelRows = BaseTrackRow
+
+export type TrackAlbumListPanelRows = BaseTrackRow & {
+  songs: TrackSongsListPanelRows[]
+}
+
+export type TrackAlbumPageColumns = BaseTrackRow & {
   duration: string
 }
 
-export const tracks: Track[] = [
+export const tracks: TrackAlbumPageColumns[] = [
   {
     id: '1',
-    image: 'https://i.scdn.co/image/ab67616d0000b27349d694203245f241a1bcaa72',
+    imageUrl:
+      'https://i.scdn.co/image/ab67616d0000b27349d694203245f241a1bcaa72',
     title: 'Moscow Mule',
     artist: 'Bad Bunny',
-    created_at: '2022-03-15',
+    release_year: '2022-03-15',
     duration: '4:05',
   },
   {
     id: '2',
-    image: 'https://i.scdn.co/image/ab67616d0000b27349d694203245f241a1bcaa72',
+    imageUrl:
+      'https://i.scdn.co/image/ab67616d0000b27349d694203245f241a1bcaa72',
     title: 'Después de la Playa',
     artist: 'Bad Bunny',
-    created_at: '2023-07-22',
+    release_year: '2023-07-22',
     duration: '3:50',
   },
   {
     id: '3',
-    image: 'https://i.scdn.co/image/ab67616d0000b27349d694203245f241a1bcaa72',
+    imageUrl:
+      'https://i.scdn.co/image/ab67616d0000b27349d694203245f241a1bcaa72',
     title: 'Me Porto Bonito',
     artist: 'Bad Bunny, Chencho Corleone',
-    created_at: '2021-11-08',
+    release_year: '2021-11-08',
     duration: '2:58',
   },
   {
     id: '4',
-    image: 'https://i.scdn.co/image/ab67616d0000b27349d694203245f241a1bcaa72',
+    imageUrl:
+      'https://i.scdn.co/image/ab67616d0000b27349d694203245f241a1bcaa72',
     title: 'Tití Me Preguntó',
     artist: 'Bad Bunny',
-    created_at: '2022-05-30',
+    release_year: '2022-05-30',
     duration: '4:03',
   },
   {
     id: '5',
-    image: 'https://i.scdn.co/image/ab67616d0000b27349d694203245f241a1bcaa72',
+    imageUrl:
+      'https://i.scdn.co/image/ab67616d0000b27349d694203245f241a1bcaa72',
     title: 'Un Ratito',
     artist: 'Bad Bunny',
-    created_at: '2023-01-17',
+    release_year: '2023-01-17',
     duration: '2:56',
   },
   {
     id: '6',
-    image: 'https://i.scdn.co/image/ab67616d0000b27349d694203245f241a1bcaa72',
+    imageUrl:
+      'https://i.scdn.co/image/ab67616d0000b27349d694203245f241a1bcaa72',
     title: 'Yo No Soy Celoso',
     artist: 'Bad Bunny',
-    created_at: '2022-09-04',
+    release_year: '2022-09-04',
     duration: '3:50',
   },
   {
     id: '7',
-    image: 'https://i.scdn.co/image/ab67616d0000b27349d694203245f241a1bcaa72',
+    imageUrl:
+      'https://i.scdn.co/image/ab67616d0000b27349d694203245f241a1bcaa72',
     title: 'Tarot',
     artist: 'Bad Bunny, Jhayco',
-    created_at: '2021-06-25',
+    release_year: '2021-06-25',
     duration: '3:57',
   },
   {
     id: '8',
-    image: 'https://i.scdn.co/image/ab67616d0000b27349d694203245f241a1bcaa72',
+    imageUrl:
+      'https://i.scdn.co/image/ab67616d0000b27349d694203245f241a1bcaa72',
     title: 'Neverita',
     artist: 'Bad Bunny',
-    created_at: '2023-04-12',
+    release_year: '2023-04-12',
     duration: '2:53',
   },
   {
     id: '9',
-    image: 'https://i.scdn.co/image/ab67616d0000b27349d694203245f241a1bcaa72',
+    imageUrl:
+      'https://i.scdn.co/image/ab67616d0000b27349d694203245f241a1bcaa72',
     title: 'La Corriente',
     artist: 'Bad Bunny, Tony Dize',
-    created_at: '2022-12-19',
+    release_year: '2022-12-19',
     duration: '3:18',
   },
   {
     id: '10',
-    image: 'https://i.scdn.co/image/ab67616d0000b27349d694203245f241a1bcaa72',
+    imageUrl:
+      'https://i.scdn.co/image/ab67616d0000b27349d694203245f241a1bcaa72',
     title: 'Yonaguni',
     artist: 'Bad Bunny',
-    created_at: '2021-04-10',
+    release_year: '2021-04-10',
     duration: '2:53',
   },
   {
     id: '11',
-    image: 'https://i.scdn.co/image/ab67616d0000b27349d694203245f241a1bcaa72',
+    imageUrl:
+      'https://i.scdn.co/image/ab67616d0000b27349d694203245f241a1bcaa72',
     title: 'Crippy',
     artist: 'Bad Bunny',
-    created_at: '2020-09-01',
+    release_year: '2020-09-01',
     duration: '3:18',
   },
 ]
