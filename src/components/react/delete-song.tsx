e3ca6415474dec0eb0c7dc52978e9fecd4997c8c
Crisops <crialeperez1835@gmail.com>
2025-06-27 13:12:27 -0500

    Actualizar el componente `DeleteSong` para recibir el `id` de la canción o álbum.
    - Se agrega la propiedad `hideCloseButton` al modal.

--- src/components/react/delete-song.tsx
+++ src/components/react/delete-song.tsx
@@ -1,9 +1,14 @@
 import { useModal } from '@/hooks/use-modal'
 import { Trash2 } from 'lucide-react'
+import type { Tables } from '@/types/database.types'
 import Button from '@/components/shared/button'
 import Modal from '@/components/shared/modal'
 
-const DeleteSong = () => {
+interface DeleteSongProps {
+  id: Tables<'songs'>['id'] | Tables<'albums'>['id']
+}
+
+const DeleteSong = ({ id }: DeleteSongProps) => {
   const { isOpen, onOpen, onOpenChange } = useModal()
 
   return (
@@ -17,8 +22,8 @@ const DeleteSong = () => {
       >
         <Trash2 size={20} />
       </Button>
-      <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
-        <div>Modal para eliminar una cancion</div>
+      <Modal isOpen={isOpen} onOpenChange={onOpenChange} hideCloseButton>
+        <div>Modal para eliminar una cancion {id}</div>
       </Modal>
     </>
   )
