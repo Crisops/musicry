aacd5468cf28825f9c331e3c9f8bc37dff98f8f8
Crisops <crialeperez1835@gmail.com>
2025-07-02 12:12:44 -0500

    Agregar funciones para obtener álbumes desde Supabase en `albums.ts`.
    - Se implementa `getAllAlbums` para recuperar todos los álbumes y `getAlbumById` para obtener un álbum específico por su ID.
    - Se maneja la gestión de errores y se asegura que se devuelvan datos adecuados en caso de fallos.

---
+++ src/lib/supabase/resources/albums.ts
@@ -0,0 +1,48 @@
+import type { Tables } from '@/types/database.types'
+import { createClient } from '@/lib/supabase/server'
+import type { AstroCookies } from 'astro'
+import type { TrackAlbumPageColumns } from '@/types/track'
+
+export const getAllAlbums = async (request: Request, cookies: AstroCookies): Promise<Tables<'albums'>[]> => {
+  try {
+    const supabase = await createClient(request, cookies)
+
+    const { data: albums, error } = await supabase.from('albums').select('*')
+
+    if (error) {
+      throw new Error('Error al obtener los álbumes')
+    }
+
+    return albums
+  } catch (error) {
+    console.error(error)
+    return []
+  }
+}
+
+export const getAlbumById = async (
+  request: Request,
+  cookies: AstroCookies,
+  id: Tables<'albums'>['id'],
+): Promise<TrackAlbumPageColumns | null> => {
+  try {
+    const supabase = await createClient(request, cookies)
+
+    const { data: album, error } = await supabase
+      .from('albums')
+      .select(
+        'id, title, artist, imageUrl, release_year:releaseYear, songs:songs!songs_albumId_fkey(id, title, artist, imageUrl, duration, release_year:created_at)',
+      )
+      .eq('id', id)
+      .single()
+
+    if (error) {
+      throw new Error('Error al obtener el álbum')
+    }
+
+    return album
+  } catch (error) {
+    console.error(error)
+    return null
+  }
+}
