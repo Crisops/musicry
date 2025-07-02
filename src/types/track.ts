d1b0c3f58408725a2f1bb00bd2f4fa624c42023d
Crisops <crialeperez1835@gmail.com>
2025-07-02 12:08:46 -0500

    Actualizar tipos en `track.ts` para reflejar cambios en la estructura de datos.
    Se renombra la clave `id` a `index` de trackAlbumPageColumns y se modifica el tipo de `TrackAlbumPageColumns` para incluir un array de canciones con duración.
    
    - Se eliminan datos de ejemplo de canciones que ya no son necesarios.

--- src/types/track.ts
+++ src/types/track.ts
@@ -15,7 +15,7 @@ export type Column = {
 
 export const trackAlbumPageColumns: Column[] = [
   {
-    key: 'id',
+    key: 'index',
     label: '#',
   },
   {
@@ -81,107 +81,7 @@ export type TrackAlbumListPanelRows = BaseTrackRow & {
 }
 
 export type TrackAlbumPageColumns = BaseTrackRow & {
-  duration: string
+  songs: (TrackSongsListPanelRows & {
+    duration: Tables<'songs'>['duration']
+  })[]
 }
-
-export const tracks: TrackAlbumPageColumns[] = [
-  {
-    id: '1',
-    imageUrl:
-      'https://i.scdn.co/image/ab67616d0000b27349d694203245f241a1bcaa72',
-    title: 'Moscow Mule',
-    artist: 'Bad Bunny',
-    release_year: '2022-03-15',
-    duration: '4:05',
-  },
-  {
-    id: '2',
-    imageUrl:
-      'https://i.scdn.co/image/ab67616d0000b27349d694203245f241a1bcaa72',
-    title: 'Después de la Playa',
-    artist: 'Bad Bunny',
-    release_year: '2023-07-22',
-    duration: '3:50',
-  },
-  {
-    id: '3',
-    imageUrl:
-      'https://i.scdn.co/image/ab67616d0000b27349d694203245f241a1bcaa72',
-    title: 'Me Porto Bonito',
-    artist: 'Bad Bunny, Chencho Corleone',
-    release_year: '2021-11-08',
-    duration: '2:58',
-  },
-  {
-    id: '4',
-    imageUrl:
-      'https://i.scdn.co/image/ab67616d0000b27349d694203245f241a1bcaa72',
-    title: 'Tití Me Preguntó',
-    artist: 'Bad Bunny',
-    release_year: '2022-05-30',
-    duration: '4:03',
-  },
-  {
-    id: '5',
-    imageUrl:
-      'https://i.scdn.co/image/ab67616d0000b27349d694203245f241a1bcaa72',
-    title: 'Un Ratito',
-    artist: 'Bad Bunny',
-    release_year: '2023-01-17',
-    duration: '2:56',
-  },
-  {
-    id: '6',
-    imageUrl:
-      'https://i.scdn.co/image/ab67616d0000b27349d694203245f241a1bcaa72',
-    title: 'Yo No Soy Celoso',
-    artist: 'Bad Bunny',
-    release_year: '2022-09-04',
-    duration: '3:50',
-  },
-  {
-    id: '7',
-    imageUrl:
-      'https://i.scdn.co/image/ab67616d0000b27349d694203245f241a1bcaa72',
-    title: 'Tarot',
-    artist: 'Bad Bunny, Jhayco',
-    release_year: '2021-06-25',
-    duration: '3:57',
-  },
-  {
-    id: '8',
-    imageUrl:
-      'https://i.scdn.co/image/ab67616d0000b27349d694203245f241a1bcaa72',
-    title: 'Neverita',
-    artist: 'Bad Bunny',
-    release_year: '2023-04-12',
-    duration: '2:53',
-  },
-  {
-    id: '9',
-    imageUrl:
-      'https://i.scdn.co/image/ab67616d0000b27349d694203245f241a1bcaa72',
-    title: 'La Corriente',
-    artist: 'Bad Bunny, Tony Dize',
-    release_year: '2022-12-19',
-    duration: '3:18',
-  },
-  {
-    id: '10',
-    imageUrl:
-      'https://i.scdn.co/image/ab67616d0000b27349d694203245f241a1bcaa72',
-    title: 'Yonaguni',
-    artist: 'Bad Bunny',
-    release_year: '2021-04-10',
-    duration: '2:53',
-  },
-  {
-    id: '11',
-    imageUrl:
-      'https://i.scdn.co/image/ab67616d0000b27349d694203245f241a1bcaa72',
-    title: 'Crippy',
-    artist: 'Bad Bunny',
-    release_year: '2020-09-01',
-    duration: '3:18',
-  },
-]
