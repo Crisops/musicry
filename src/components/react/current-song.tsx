5e9d895b4f62f6f733cea5bb124e252b8cbd7cae
Crisops <crialeperez1835@gmail.com>
2025-07-08 12:26:30 -0500

    Actualizar el componente `CurrentSong` para integrar el hook `usePlaySong` y gestionar dinámicamente la información de la canción actual.
    - Se eliminan las propiedades estáticas y se utiliza el estado de `currentSong` para mostrar el título, artista e imagen de la canción.
    - Se optimiza la visualización de la imagen de la canción actual, asegurando que solo se muestre si está disponible.

--- src/components/react/current-song.tsx
+++ src/components/react/current-song.tsx
@@ -1,24 +1,24 @@
-interface CurrentSongProps {
-  title: string
-  artist: string
-  imageUrl: string
-}
+import { usePlaySong } from '@/hooks/use-store'
 
-const CurrentSong = ({ title, artist, imageUrl }: CurrentSongProps) => {
+const CurrentSong = () => {
+  const currentSong = usePlaySong((state) => state.currentSong)
+
   return (
     <article className="flex w-80 items-center gap-1.5 py-1.5">
       <div className="relative aspect-square h-auto w-11">
-        <img
-          className="absolute h-full w-full rounded-md object-cover"
-          src={imageUrl}
-          alt={title}
-          width={44}
-          height={44}
-        />
+        {currentSong?.song?.imageUrl && (
+          <img
+            className="absolute h-full w-full rounded-md object-cover"
+            src={currentSong.song.imageUrl}
+            alt={`${currentSong.song.title} - ${currentSong.song.artist}`}
+            width={44}
+            height={44}
+          />
+        )}
       </div>
       <div className="flex-initial flex-col items-start justify-center">
-        <h4 className="text-sealsalt text-small font-semibold">{title}</h4>
-        <p className="text-gray-dim text-xs">{artist}</p>
+        <h4 className="text-sealsalt text-small font-semibold">{currentSong?.song?.title}</h4>
+        <p className="text-gray-dim text-xs">{currentSong?.song?.artist}</p>
       </div>
     </article>
   )
