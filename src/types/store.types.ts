c7256b2dbfd2b6e063e0b2585546a7c0f1b9db00
Crisops <crialeperez1835@gmail.com>
2025-07-08 12:08:59 -0500

    Agregar tipos para la gestión de reproducción de canciones en `store.types.ts`.
    - Se define la estructura de `PlaySongStore` y `PlaySongCurrent`, incluyendo métodos para controlar la canción actual, el estado de reproducción, el volumen y la repetición.

--- src/types/store.types.ts
+++ src/types/store.types.ts
@@ -1,4 +1,28 @@
+import type { Tables } from '@/types/database.types'
+
 export type UserAuthStore = {
   user: User | null
   setUser: (user: User | null) => void
+}
+
+type Song = Omit<Tables<'songs'>, 'albumId' | 'created_at'> & {
+  album: {
+    id: Tables<'albums'>['id']
+  }
+}
+
+export type PlaySongCurrent = {
+  song: Song | null
+  playlist: Song[]
+}
+
+export type PlaySongStore = {
+  currentSong: PlaySongCurrent | null
+  isPlaying: boolean
+  volume: number
+  repeat: 'all' | 'one' | 'none'
+  setCurrentSong: (song: PlaySongCurrent | null) => void
+  setIsPlaying: (isPlaying: boolean) => void
+  setVolume: (volume: number) => void
+  setRepeat: (repeat: 'all' | 'one' | 'none') => void
 }
