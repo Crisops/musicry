3c83bfe73c3792f6439b9693db58e4aee3f123bc
Crisops <crialeperez1835@gmail.com>
2025-07-08 12:52:38 -0500

    Actualizar el componente `featured-songs-list` para obtener canciones aleatorias y corregir el nombre de la propiedad de imagen.
    - Se reemplaza la función `getAllSongs` por `getAllSongsRandom` para obtener un número limitado de canciones aleatorias.
    - Se corrige el nombre de la propiedad `imageUrl` a `imageurl` en el mapeo de canciones.

--- src/lib/supabase/resources/songs.ts
+++ src/lib/supabase/resources/songs.ts
@@ -1,12 +1,22 @@
-import type { Tables } from '@/types/database.types'
 import { createClient } from '@/lib/supabase/server'
 import type { AstroCookies } from 'astro'
 
-export const getAllSongs = async (request: Request, cookies: AstroCookies): Promise<Tables<'songs'>[]> => {
+type Song = {
+  id: string
+  title: string
+  artist: string
+  imageurl: string
+  audiourl: string
+  duration: number
+  created_at: string
+  albumid: string
+}
+
+export const getAllSongsRandom = async (request: Request, cookies: AstroCookies, limit?: number): Promise<Song[]> => {
   try {
     const supabase = await createClient(request, cookies)
 
-    const { data: songs, error } = await supabase.from('songs').select('*')
+    const { data: songs, error } = await supabase.rpc('get_random_songs_with_album', { song_limit: limit })
 
     if (error) {
       throw new Error('Error al obtener las canciones')
