fd9b142581395185f5cbbe84956cbec73ed0742e
Crisops <crialeperez1835@gmail.com>
2025-07-08 12:23:44 -0500

    Actualizar el componente `ProgressSlider` para integrar el hook `useAudioSeek` y mejorar la gestión de la búsqueda de tiempo en la reproducción de audio.
    - Se añaden propiedades para manejar el tiempo de reproducción y la duración del audio.
    - Se optimizan los controles del slider para reflejar el tiempo actual y permitir la búsqueda de manera más eficiente.

--- src/components/react/progress-slider.tsx
+++ src/components/react/progress-slider.tsx
@@ -1,25 +1,38 @@
 import { Slider } from '@heroui/slider'
-const ProgressSlider = () => {
+import { useAudioSeek } from '@/hooks/use-audio-seek'
+import { formatDuration } from '@/utils/format-duration'
+
+interface ProgressSliderProps {
+  audioRef: React.RefObject<HTMLAudioElement | null>
+}
+
+const ProgressSlider = ({ audioRef }: ProgressSliderProps) => {
+  const { displayTime, duration, handleSeekChange, handleSeekEnd } = useAudioSeek({ audioRef })
+
   return (
     <Slider
-      startContent="00:00"
-      endContent="03:00"
-      minValue={0}
-      maxValue={100}
       aria-label="Control de reproducción"
+      defaultValue={[0]}
+      value={[displayTime]}
+      startContent={formatDuration(displayTime)}
+      endContent={formatDuration(duration)}
+      minValue={0}
+      maxValue={duration}
       classNames={{
         filler: ['bg-blue-argentinian'],
         thumb: [
-          'transition-opacity opacity-0 duration-200 group-hover:opacity-100 w-2 h-2 cursor-default after:w-2 after:h-2 data-[pressed=true]:cursor-default',
+          'transition-opacity opacity-0 duration-200 group-hover:opacity-100 w-2 h-2 cursor-default after:w-2 after:h-2 data-[pressed=true]:cursor-default data-[dragging=true]:cursor-default',
         ],
         track: [
-          'group bg-blue-oxford data-[fill-start=true]:border-s-blue-argentinian data-[fill-end=true]:border-e-blue-argentinian border-x-[calc(theme(spacing.0)/2)]',
+          'group bg-blue-oxford data-[fill-start=true]:border-s-blue-argentinian data-[fill-end=true]:border-e-blue-argentinian border-x-[calc(theme(spacing.0)/2)] ',
         ],
         startContent: ['text-platinum text-xs'],
         endContent: ['text-platinum text-xs'],
       }}
       disableThumbScale
       size="sm"
+      onChange={handleSeekChange}
+      onChangeEnd={handleSeekEnd}
     />
   )
 }
