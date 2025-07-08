a0246289f55f508da7b0cc08f61bd8c9f74d0e83
Crisops <crialeperez1835@gmail.com>
2025-07-08 12:49:19 -0500

    Actualizar el componente `ButtonPlay` para integrar el hook `usePlaySong` y mejorar la gestión de la reproducción de canciones.
    - Se añade la lógica para manejar la reproducción y pausa de la canción actual.
    - Se implementa la obtención de datos de la canción desde la API y se gestionan los errores con un mensaje de error.
    - Se optimiza el renderizado del botón para mostrar el icono correspondiente según el estado de reproducción.

--- src/components/react/button-play.tsx
+++ src/components/react/button-play.tsx
@@ -1,17 +1,56 @@
+import { memo } from 'react'
+import { useShallow } from 'zustand/react/shallow'
+import { Pause, Play } from 'lucide-react'
+import axiosClient from '@/lib/axios/client'
+import { usePlaySong } from '@/hooks/use-store'
+import type { PlaySongCurrent } from '@/types/store.types'
+import type { Tables } from '@/types/database.types'
+import { Toast } from '@/utils/toast'
 import type { ButtonProps } from '@heroui/button'
-import { Play } from 'lucide-react'
 import Button from '@/components/shared/button'
 
 interface ButtonPropsPlay extends ButtonProps {
+  id: Tables<'songs'>['id'] | Tables<'albums'>['id']
   sizeIcon?: number
 }
 
-const ButtonPlay = ({ sizeIcon = 16, ...props }: ButtonPropsPlay) => {
+const ButtonPlay = ({ id, sizeIcon = 16, ...props }: ButtonPropsPlay) => {
+  const { isCurrentSong, isPlayingPlayList, setCurrentSong, setIsPlaying } = usePlaySong(
+    useShallow((s) => {
+      const isCurrentSong = s.currentSong?.song?.id === id || s.currentSong?.song?.album?.id === id
+      return {
+        isCurrentSong,
+        isPlayingPlayList: s.isPlaying && isCurrentSong,
+        setCurrentSong: s.setCurrentSong,
+        setIsPlaying: s.setIsPlaying,
+      }
+    }),
+  )
+
+  const handlePlaySong = async () => {
+    if (isCurrentSong) return setIsPlaying(!isPlayingPlayList)
+    const result = await axiosClient.get<PlaySongCurrent>('/api/songs', { params: { id } })
+    if (!result.success) {
+      console.error(result.error)
+      Toast().error({
+        title: '¡Ups!',
+        description: result.error.message,
+      })
+      return
+    }
+    setIsPlaying(true)
+    setCurrentSong(result.data)
+  }
+
   return (
-    <Button {...props} isIconOnly variant="solid">
-      <Play fill="currentColor" size={sizeIcon} color="currentColor" />
+    <Button onPress={handlePlaySong} {...props} isIconOnly variant="solid">
+      {isPlayingPlayList ? (
+        <Pause fill="currentColor" size={sizeIcon} color="currentColor" />
+      ) : (
+        <Play fill="currentColor" size={sizeIcon} color="currentColor" />
+      )}
     </Button>
   )
 }
 
-export default ButtonPlay
+export default memo(ButtonPlay)
