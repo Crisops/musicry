cff8b9449f77eef232fd42c64797a400a0a9c8b1
Crisops <crialeperez1835@gmail.com>
2025-07-08 12:24:28 -0500

    Actualizar el componente `PlaybackControls` para integrar el hook `usePlaySong` y mejorar la gestión de los controles de reproducción.
    - Se añaden propiedades para manejar la reproducción de la canción anterior y siguiente.
    - Se implementa la lógica para alternar entre reproducción y pausa, y se gestiona la repetición de canciones de manera dinámica.
    - Se optimizan los iconos de control para reflejar el estado actual de reproducción.

--- src/components/react/playback-controls.tsx
+++ src/components/react/playback-controls.tsx
@@ -1,25 +1,41 @@
-import { Play, Shuffle, SkipBack, SkipForward, Repeat } from 'lucide-react'
-import PlaybackIconButton from './playback-icon-button'
+import { Play, Shuffle, SkipBack, SkipForward, Repeat, Pause, Repeat1 } from 'lucide-react'
+import { usePlaySong } from '@/hooks/use-store'
+import { usePlaybackControls } from '@/hooks/use-playback-controls'
+import PlaybackIconButton from '@/components/react/playback-icon-button'
 
-const PlaybackControls = () => {
+interface PlaybackControlsProps {
+  playNext: () => void
+  playPrevious: () => void
+}
+
+const PlaybackControls = ({ playNext, playPrevious }: PlaybackControlsProps) => {
+  const { isPlaying, repeat } = usePlaySong((state) => state)
+  const { handlePlaying, handleRepeat } = usePlaybackControls()
   return (
     <div className="flex w-full items-center justify-center gap-6">
       <PlaybackIconButton>
         <Shuffle size={16} />
       </PlaybackIconButton>
       <div className="flex items-center justify-center gap-3">
-        <PlaybackIconButton>
+        <PlaybackIconButton onPress={playPrevious} aria-label="Reproducir canción anterior">
           <SkipBack fill="currentColor" size={18} />
         </PlaybackIconButton>
-        <PlaybackIconButton className="bg-platinum h-10 w-10 min-w-10 text-black">
-          <Play fill="currentColor" size={16} color="currentColor" />
+        <PlaybackIconButton className="bg-platinum h-10 w-10 min-w-10 text-black" onPress={handlePlaying}>
+          {isPlaying ? (
+            <Pause fill="currentColor" size={16} color="currentColor" />
+          ) : (
+            <Play fill="currentColor" size={16} color="currentColor" />
+          )}
         </PlaybackIconButton>
-        <PlaybackIconButton>
+        <PlaybackIconButton onPress={playNext} aria-label="Reproducir canción siguiente">
           <SkipForward fill="currentColor" size={18} />
         </PlaybackIconButton>
       </div>
-      <PlaybackIconButton>
-        <Repeat size={16} />
+      <PlaybackIconButton
+        className={`${repeat !== 'none' ? 'text-blue-argentinian' : 'text-white'}`}
+        onPress={handleRepeat}
+      >
+        {repeat !== 'all' ? <Repeat color="currentColor" size={16} /> : <Repeat1 color="currentColor" size={16} />}
       </PlaybackIconButton>
     </div>
   )
