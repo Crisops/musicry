177ac0a4769cae1320c2b8373e8365332f10eb25
Crisops <crialeperez1835@gmail.com>
2025-07-10 18:09:32 -0500

    Actualizar el componente `Player` para mostrar la canción actual solo si está disponible.

--- src/components/react/player.tsx
+++ src/components/react/player.tsx
@@ -1,4 +1,5 @@
 import { useRef } from 'react'
+import { usePlaySong } from '@/hooks/use-store'
 import { useCurrentSong } from '@/hooks/use-current-song'
 import CurrentSong from '@/components/react/current-song'
 import ProgressSlider from '@/components/react/progress-slider'
@@ -8,21 +9,26 @@ import VolumenControl from '@/components/react/volumen-control'
 const Player = () => {
   const audioRef = useRef<HTMLAudioElement>(null)
   const { playNext, playPrevious } = useCurrentSong({ audioRef })
+  const currentSong = usePlaySong((state) => state.currentSong)
 
   return (
-    <section className="relative hidden h-full w-full items-center justify-between lg:flex">
-      <audio ref={audioRef} />
-      <CurrentSong />
-      <div className="flex flex-grow basis-0 items-center justify-center">
-        <div className="flex w-full max-w-2xl flex-col items-center justify-center">
-          <PlaybackControls playNext={playNext} playPrevious={playPrevious} />
-          <ProgressSlider audioRef={audioRef} />
-        </div>
-      </div>
-      <div className="hidden w-80 pr-3 2xl:block">
-        <VolumenControl audioRef={audioRef} />
-      </div>
-    </section>
+    <>
+      {currentSong?.song && (
+        <section className="relative hidden h-full w-full items-center justify-between lg:flex">
+          <audio ref={audioRef} />
+          <CurrentSong />
+          <div className="flex flex-grow basis-0 items-center justify-center">
+            <div className="flex w-full max-w-2xl flex-col items-center justify-center">
+              <PlaybackControls playNext={playNext} playPrevious={playPrevious} />
+              <ProgressSlider audioRef={audioRef} />
+            </div>
+          </div>
+          <div className="hidden w-80 pr-3 2xl:block">
+            <VolumenControl audioRef={audioRef} />
+          </div>
+        </section>
+      )}
+    </>
   )
 }
 
