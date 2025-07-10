ec173cacc45d2dda4fdb7039a4a0a0b38e6251a5
Crisops <crialeperez1835@gmail.com>
2025-07-10 18:23:47 -0500

    Actualizar el componente `ButtonPlay` para integrar la lógica de aleatorización en la selección de canciones.
    - Se añaden propiedades para manejar la fuente de la canción y la lista de reproducción aleatoria.
    - Se optimiza la lógica de reproducción al considerar la lista de reproducción actual y la aleatorización.

--- src/components/react/button-play.tsx
+++ src/components/react/button-play.tsx
@@ -1,10 +1,11 @@
 import { memo } from 'react'
 import { useShallow } from 'zustand/react/shallow'
 import { Pause, Play } from 'lucide-react'
+import type { Tables } from '@/types/database.types'
+import type { PlaySongCurrent, Song } from '@/types/store.types'
 import axiosClient from '@/lib/axios/client'
 import { usePlaySong } from '@/hooks/use-store'
-import type { PlaySongCurrent } from '@/types/store.types'
-import type { Tables } from '@/types/database.types'
+import { shuffleMix } from '@/utils/shuffle-mix'
 import { Toast } from '@/utils/toast'
 import type { ButtonProps } from '@heroui/button'
 import Button from '@/components/shared/button'
@@ -12,24 +13,53 @@ import Button from '@/components/shared/button'
 interface ButtonPropsPlay extends ButtonProps {
   id: Tables<'songs'>['id'] | Tables<'albums'>['id']
   sizeIcon?: number
+  source: 'featured' | 'made-for-you' | 'playlist' | 'albums'
 }
 
-const ButtonPlay = ({ id, sizeIcon = 16, ...props }: ButtonPropsPlay) => {
-  const { isCurrentSong, isPlayingPlayList, setCurrentSong, setIsPlaying } = usePlaySong(
+const ButtonPlay = ({ id, sizeIcon = 16, source, ...props }: ButtonPropsPlay) => {
+  const {
+    currentSong,
+    isCurrentSong,
+    isPlayingPlayList,
+    shuffle,
+    shufflePlaylist,
+    setCurrentSong,
+    setIsPlaying,
+    setShufflePlaylist,
+  } = usePlaySong(
     useShallow((s) => {
-      const isCurrentSong = s.currentSong?.song?.id === id || s.currentSong?.song?.album?.id === id
+      const isCurrentSong =
+        s.currentSong?.song?.id === id || s.currentSong?.song?.album?.id === id || s.shufflePlaylist?.song?.id === id
       return {
+        currentSong: s.currentSong,
         isCurrentSong,
         isPlayingPlayList: s.isPlaying && isCurrentSong,
+        shuffle: s.shuffle,
+        shufflePlaylist: s.shufflePlaylist,
         setCurrentSong: s.setCurrentSong,
         setIsPlaying: s.setIsPlaying,
+        setShufflePlaylist: s.setShufflePlaylist,
       }
     }),
   )
 
   const handlePlaySong = async () => {
     if (isCurrentSong) return setIsPlaying(!isPlayingPlayList)
-    const result = await axiosClient.get<PlaySongCurrent>('/api/songs', { params: { id } })
+    if (source === 'playlist') {
+      const list = shuffle ? shufflePlaylist?.playlist : currentSong?.playlist
+      const selectedSong = list?.find((song) => song.id === id)
+      console.log('list', list)
+      if (selectedSong && list) {
+        if (shuffle) {
+          setShufflePlaylist({ song: selectedSong, playlist: list })
+        } else {
+          setCurrentSong({ song: selectedSong, playlist: list })
+        }
+        setIsPlaying(true)
+        return
+      }
+    }
+    const result = await axiosClient.get<PlaySongCurrent>(`/api/songs/${source}`, { params: { id } })
     if (!result.success) {
       console.error(result.error)
       Toast().error({
@@ -38,8 +68,13 @@ const ButtonPlay = ({ id, sizeIcon = 16, ...props }: ButtonPropsPlay) => {
       })
       return
     }
-    setIsPlaying(true)
     setCurrentSong(result.data)
+    if (shuffle && result.data) {
+      console.log('result.data', result.data)
+      const shuffled = shuffleMix<Song>(result.data.playlist)
+      setShufflePlaylist({ song: result.data.song, playlist: shuffled })
+    }
+    setIsPlaying(true)
   }
 
   return (
