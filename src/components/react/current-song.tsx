e0b7d6725487638ddaddc663590564bf25ae967b
Crisops <crialeperez1835@gmail.com>
2025-07-10 18:08:34 -0500

    Actualizar el componente `CurrentSong` para utilizar la lógica de aleatorización en la selección de canciones.
    - Se modifica la obtención de la canción actual para considerar la propiedad `shuffle` y se optimiza la visualización de la información de la canción.

--- src/components/react/current-song.tsx
+++ src/components/react/current-song.tsx
@@ -1,24 +1,29 @@
+import { useShallow } from 'zustand/react/shallow'
 import { usePlaySong } from '@/hooks/use-store'
 
 const CurrentSong = () => {
-  const currentSong = usePlaySong((state) => state.currentSong)
+  const { song } = usePlaySong(
+    useShallow((s) => ({
+      song: s.shuffle ? s.shufflePlaylist?.song : s.currentSong?.song,
+    })),
+  )
 
   return (
     <article className="flex w-80 items-center gap-1.5 py-1.5">
       <div className="relative aspect-square h-auto w-11">
-        {currentSong?.song?.imageUrl && (
+        {song?.imageUrl && (
           <img
             className="absolute h-full w-full rounded-md object-cover"
-            src={currentSong.song.imageUrl}
-            alt={`${currentSong.song.title} - ${currentSong.song.artist}`}
+            src={song.imageUrl}
+            alt={`${song.title} - ${song.artist}`}
             width={44}
             height={44}
           />
         )}
       </div>
       <div className="flex-initial flex-col items-start justify-center">
-        <h4 className="text-sealsalt text-small font-semibold">{currentSong?.song?.title}</h4>
-        <p className="text-gray-dim text-xs">{currentSong?.song?.artist}</p>
+        <h4 className="text-sealsalt text-small font-semibold">{song?.title}</h4>
+        <p className="text-gray-dim text-xs">{song?.artist}</p>
       </div>
     </article>
   )
