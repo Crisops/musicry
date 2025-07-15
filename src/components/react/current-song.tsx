d25467d1e81d8890c1b0f45fe84e7e7b500e03c3
Crisops <crialeperez1835@gmail.com>
2025-07-15 17:42:30 -0500

    Actualizar el componente `CurrentSong` para aceptar propiedades personalizadas y mejorar la lógica de visualización.
    - Se añade la interfaz `CurrentSongProps` para permitir la personalización de la clase CSS.
    - Se modifica la clase del artículo para incluir la clase personalizada y se ajustan los estilos de texto para mejorar la legibilidad.

--- src/components/react/current-song.tsx
+++ src/components/react/current-song.tsx
@@ -1,7 +1,13 @@
+import type { HTMLProps } from 'react'
 import { useShallow } from 'zustand/react/shallow'
+import { cn } from '@/lib/utils'
 import { usePlaySong } from '@/hooks/use-store'
 
-const CurrentSong = () => {
+interface CurrentSongProps {
+  className?: HTMLProps<HTMLElement>['className']
+}
+
+const CurrentSong = ({ className }: CurrentSongProps) => {
   const { song } = usePlaySong(
     useShallow((s) => ({
       song: s.shuffle ? s.shufflePlaylist?.song : s.currentSong?.song,
@@ -9,7 +15,7 @@ const CurrentSong = () => {
   )
 
   return (
-    <article className="flex w-80 items-center gap-1.5 py-1.5">
+    <article className={cn('flex', className)}>
       <div className="relative aspect-square h-auto w-11">
         {song?.imageUrl && (
           <img
@@ -22,8 +28,8 @@ const CurrentSong = () => {
         )}
       </div>
       <div className="flex-initial flex-col items-start justify-center">
-        <h4 className="text-sealsalt text-small font-semibold">{song?.title}</h4>
-        <p className="text-gray-dim text-xs">{song?.artist}</p>
+        <h4 className="text-small font-semibold text-current">{song?.title}</h4>
+        <p className="text-xs text-current">{song?.artist}</p>
       </div>
     </article>
   )
