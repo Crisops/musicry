0196789852bfc1d0c58696e18e4562c11226a6fe
Crisops <crialeperez1835@gmail.com>
2025-07-15 17:43:28 -0500

    Actualizar el componente `Player` para integrar el contexto de audio y mejorar la personalización de los controles de reproducción.
    - Se reemplaza el uso de `useRef` por `useAudioContext` para gestionar la referencia de audio.
    - Se añaden clases personalizadas a `CurrentSong`, `PlaybackControls` y `ProgressSlider` para mejorar la flexibilidad y el estilo del reproductor.

--- src/components/react/player.tsx
+++ src/components/react/player.tsx
@@ -1,14 +1,14 @@
-import { useRef } from 'react'
 import { useAuth, usePlaySong } from '@/hooks/use-store'
 import { useCurrentSong } from '@/hooks/use-current-song'
 import CurrentSong from '@/components/react/current-song'
 import ProgressSlider from '@/components/react/progress-slider'
 import PlaybackControls from '@/components/react/playback-controls'
 import VolumenControl from '@/components/react/volumen-control'
+import { useAudioContext } from '@/hooks/use-audio-context'
 
 const Player = () => {
   const user = useAuth((state) => state.user)
-  const audioRef = useRef<HTMLAudioElement>(null)
+  const { audioRef } = useAudioContext()
   const { playNext, playPrevious } = useCurrentSong({ audioRef })
   const currentSong = usePlaySong((state) => state.currentSong)
 
@@ -17,11 +17,24 @@ const Player = () => {
       {currentSong?.song && user && (
         <section className="relative hidden h-full w-full items-center justify-between lg:flex">
           <audio ref={audioRef} />
-          <CurrentSong />
+          <CurrentSong className="[&_h4]:text-sealsalt [&_p]:text-gray-dim w-80 items-center gap-1.5 py-1.5" />
           <div className="flex flex-grow basis-0 items-center justify-center">
             <div className="flex w-full max-w-2xl flex-col items-center justify-center">
-              <PlaybackControls playNext={playNext} playPrevious={playPrevious} />
-              <ProgressSlider audioRef={audioRef} />
+              <PlaybackControls playNext={playNext} playPrevious={playPrevious} className="gap-6" />
+              <ProgressSlider
+                audioRef={audioRef}
+                classNames={{
+                  filler: ['bg-blue-argentinian'],
+                  thumb: [
+                    'transition-opacity opacity-0 duration-200 group-hover:opacity-100 w-2 h-2 cursor-default after:w-2 after:h-2 data-[pressed=true]:cursor-default data-[dragging=true]:cursor-default',
+                  ],
+                  track: [
+                    'group bg-blue-oxford data-[fill-start=true]:border-s-blue-argentinian data-[fill-end=true]:border-e-blue-argentinian border-x-[calc(theme(spacing.0)/2)] ',
+                  ],
+                  startContent: ['text-platinum text-xs'],
+                  endContent: ['text-platinum text-xs'],
+                }}
+              />
             </div>
           </div>
           <div className="hidden w-80 pr-3 2xl:block">
