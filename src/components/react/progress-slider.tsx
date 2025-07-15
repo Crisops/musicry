0f263f861cd68e151cb6ec102711f8b69640eb56
Crisops <crialeperez1835@gmail.com>
2025-07-15 17:41:46 -0500

    Actualizar el componente `ProgressSlider` para extender las propiedades del slider y gestionar el estado de búsqueda de audio.
    -Se añaden las propiedades `disabledContent` y `audioSeekState`, mejorando la lógica de visualización del tiempo de reproducción y la duración.

--- src/components/react/progress-slider.tsx
+++ src/components/react/progress-slider.tsx
@@ -1,34 +1,27 @@
-import { Slider } from '@heroui/slider'
+import { Slider, type SliderProps } from '@heroui/slider'
 import { useAudioSeek } from '@/hooks/use-audio-seek'
 import { formatDuration } from '@/utils/format-duration'
 
-interface ProgressSliderProps {
+interface ProgressSliderProps extends SliderProps {
   audioRef: React.RefObject<HTMLAudioElement | null>
+  disabledContent?: boolean
+  audioSeekState?: ReturnType<typeof useAudioSeek>
 }
 
-const ProgressSlider = ({ audioRef }: ProgressSliderProps) => {
-  const { displayTime, duration, handleSeekChange, handleSeekEnd } = useAudioSeek({ audioRef })
+const ProgressSlider = ({ audioRef, disabledContent = false, audioSeekState, ...props }: ProgressSliderProps) => {
+  const internalAudioSeekState = useAudioSeek({ audioRef })
+  const { displayTime, duration, handleSeekChange, handleSeekEnd } = audioSeekState || internalAudioSeekState
 
   return (
     <Slider
+      {...props}
       aria-label="Control de reproducción"
       defaultValue={[0]}
       value={[displayTime]}
-      startContent={formatDuration(displayTime)}
-      endContent={formatDuration(duration)}
+      startContent={disabledContent ? null : formatDuration(displayTime)}
+      endContent={disabledContent ? null : formatDuration(duration)}
       minValue={0}
       maxValue={duration}
-      classNames={{
-        filler: ['bg-blue-argentinian'],
-        thumb: [
-          'transition-opacity opacity-0 duration-200 group-hover:opacity-100 w-2 h-2 cursor-default after:w-2 after:h-2 data-[pressed=true]:cursor-default data-[dragging=true]:cursor-default',
-        ],
-        track: [
-          'group bg-blue-oxford data-[fill-start=true]:border-s-blue-argentinian data-[fill-end=true]:border-e-blue-argentinian border-x-[calc(theme(spacing.0)/2)] ',
-        ],
-        startContent: ['text-platinum text-xs'],
-        endContent: ['text-platinum text-xs'],
-      }}
       disableThumbScale
       size="sm"
       onChange={handleSeekChange}
