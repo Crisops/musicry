403b33f64308a77d0a6d330d28ed9d52a1e3f283
Crisops <crialeperez1835@gmail.com>
2025-07-15 17:42:59 -0500

    Actualizar el componente `PlaybackControls` para aceptar propiedades personalizadas y mejorar la flexibilidad de los íconos.
    - Se añaden las propiedades `sizeIcon` y `className` a la interfaz `PlaybackControlsProps` para permitir personalización.
    - Se ajustan los tamaños de los íconos en función de las nuevas propiedades, mejorando la adaptabilidad del componente.

--- src/components/react/playback-controls.tsx
+++ src/components/react/playback-controls.tsx
@@ -1,5 +1,7 @@
+import type { HTMLProps } from 'react'
 import { Play, Shuffle, SkipBack, SkipForward, Repeat, Pause, Repeat1 } from 'lucide-react'
 import { useShallow } from 'zustand/react/shallow'
+import { cn } from '@/lib/utils'
 import { usePlaySong } from '@/hooks/use-store'
 import { usePlaybackControls } from '@/hooks/use-playback-controls'
 import PlaybackIconButton from '@/components/react/playback-icon-button'
@@ -7,9 +9,11 @@ import PlaybackIconButton from '@/components/react/playback-icon-button'
 interface PlaybackControlsProps {
   playNext: () => void
   playPrevious: () => void
+  sizeIcon?: Partial<Record<'shuffle' | 'play-previous' | 'play-next' | 'repeat' | 'play-pause', number>>
+  className?: HTMLProps<HTMLDivElement>['className']
 }
 
-const PlaybackControls = ({ playNext, playPrevious }: PlaybackControlsProps) => {
+const PlaybackControls = ({ playNext, playPrevious, sizeIcon, className }: PlaybackControlsProps) => {
   const { isPlaying, repeat, shuffle } = usePlaySong(
     useShallow((state) => ({
       isPlaying: state.isPlaying,
@@ -19,30 +23,43 @@ const PlaybackControls = ({ playNext, playPrevious }: PlaybackControlsProps) => 
   )
   const { handlePlaying, handleRepeat, handleShuffle } = usePlaybackControls()
   return (
-    <div className="flex w-full items-center justify-center gap-6">
-      <PlaybackIconButton onPress={handleShuffle} className={`${shuffle ? 'text-blue-argentinian' : 'text-white'}`}>
-        <Shuffle size={16} color="currentColor" />
+    <div className={cn('flex w-full items-center justify-center', className)}>
+      <PlaybackIconButton
+        isIconOnly
+        onPress={handleShuffle}
+        className={`${shuffle ? 'text-blue-argentinian' : 'text-white'}`}
+      >
+        <Shuffle size={sizeIcon?.['shuffle'] ?? 16} color="currentColor" />
       </PlaybackIconButton>
-      <div className="flex items-center justify-center gap-3">
-        <PlaybackIconButton onPress={playPrevious} aria-label="Reproducir canción anterior">
-          <SkipBack fill="currentColor" size={18} />
+      <div className="flex items-center justify-center gap-6 lg:gap-4">
+        <PlaybackIconButton isIconOnly onPress={playPrevious} aria-label="Reproducir canción anterior">
+          <SkipBack fill="currentColor" size={sizeIcon?.['play-previous'] ?? 18} />
         </PlaybackIconButton>
-        <PlaybackIconButton className="bg-platinum h-10 w-10 min-w-10 text-black" onPress={handlePlaying}>
+        <PlaybackIconButton
+          isIconOnly
+          className="bg-platinum h-14 w-14 min-w-14 text-black lg:h-10 lg:w-10 lg:min-w-10"
+          onPress={handlePlaying}
+        >
           {isPlaying ? (
-            <Pause fill="currentColor" size={16} color="currentColor" />
+            <Pause fill="currentColor" size={sizeIcon?.['play-pause'] ?? 16} color="currentColor" />
           ) : (
-            <Play fill="currentColor" size={16} color="currentColor" />
+            <Play fill="currentColor" size={sizeIcon?.['play-pause'] ?? 16} color="currentColor" />
           )}
         </PlaybackIconButton>
-        <PlaybackIconButton onPress={playNext} aria-label="Reproducir canción siguiente">
-          <SkipForward fill="currentColor" size={18} />
+        <PlaybackIconButton isIconOnly onPress={playNext} aria-label="Reproducir canción siguiente">
+          <SkipForward fill="currentColor" size={sizeIcon?.['play-next'] ?? 18} />
         </PlaybackIconButton>
       </div>
       <PlaybackIconButton
+        isIconOnly
         className={`${repeat !== 'none' ? 'text-blue-argentinian' : 'text-white'}`}
         onPress={handleRepeat}
       >
-        {repeat !== 'all' ? <Repeat color="currentColor" size={16} /> : <Repeat1 color="currentColor" size={16} />}
+        {repeat !== 'all' ? (
+          <Repeat color="currentColor" size={sizeIcon?.['repeat'] ?? 16} />
+        ) : (
+          <Repeat1 color="currentColor" size={sizeIcon?.['repeat'] ?? 16} />
+        )}
       </PlaybackIconButton>
     </div>
   )
