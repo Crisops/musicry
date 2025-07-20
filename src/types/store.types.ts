046d36e3999a3b58ee7f4aca1f6c295245fc86f8
Crisops <crialeperez1835@gmail.com>
2025-07-20 18:00:36 -0500

    Agregar tipos para la gestión de presencia de usuarios en tiempo real en `store.types.ts`.
    - Se introduce la interfaz `UserPresenceTrack` para representar la información de los usuarios en línea.
    - Se añade la interfaz `UserPresenceStore` para gestionar el estado de los usuarios en la sala, incluyendo métodos para cargar y actualizar la presencia de usuarios.

--- src/types/store.types.ts
+++ src/types/store.types.ts
@@ -1,4 +1,5 @@
 import type { Tables } from '@/types/database.types'
+import type { RealtimeChannel } from '@supabase/supabase-js'
 
 export type UserAuthStore = {
   user: User
@@ -31,4 +32,29 @@ export type PlaySongStore = {
   setShuffle: (shuffle: boolean) => void
   setShufflePlaylist: (shufflePlaylist: PlaySongCurrent) => void
   setOriginalPlaylist: (originalPlaylist: Song[] | null) => void
+}
+
+export type UserPresenceTrack = {
+  id: Tables<'users'>['id']
+  full_name: Tables<'users'>['full_name']
+  avatar: Tables<'users'>['avatar_url']
+  current_song: Omit<Song, 'audioUrl' | 'imageUrl' | 'album'> | null
+  last_seen: Tables<'users'>['last_seen']
+}
+
+export type UserPresence = Record<string, UserPresenceTrack[]> | null
+
+export type UserPresenceStore = {
+  onlineUsers: UserPresence // Usuarios en tiempo real
+  allUsersInRoom: Record<string, UserPresenceTrack & { isOnline: boolean }>
+  channel: RealtimeChannel | null
+  isConnected: boolean
+  currentSong: Song | null
+  currenUserId: Tables<'users'>['id'] | null
+  hasLoadedUsers: boolean
+  loadInitialUsers: () => Promise<void>
+  initPresence: (user: User) => void
+  updateCurrentSong: (song: Song, user: User) => void
+  setOnlineUsers: (onlineUsers: UserPresence) => void
+  disconnect: () => void
 }
