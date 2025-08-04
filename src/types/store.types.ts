9ee2d74eda964bb8ecb728bbc785aaaef9c05a46
Crisops <crialeperez1835@gmail.com>
2025-08-04 17:16:03 -0500

    Agregar nuevos tipos para la gestión de mensajes directos en `store.types.ts`.
    - Se introduce el tipo `DMMessage` para representar los mensajes directos.
    - Se añade el tipo `DMBroadcastPayLoad` para manejar la carga útil de mensajes.
    - Se crea el tipo `DMChatState` para gestionar el estado de los chats directos.
    - Se implementa el tipo `TypingEvent` para representar el estado de escritura de los usuarios.
    - Se define el tipo `DMStore` para gestionar los chats activos y las interacciones del usuario.

--- src/types/store.types.ts
+++ src/types/store.types.ts
@@ -45,7 +45,7 @@ export type UserPresenceTrack = {
 export type UserPresence = Record<string, UserPresenceTrack[]> | null
 
 export type UserPresenceStore = {
-  onlineUsers: UserPresence // Usuarios en tiempo real
+  onlineUsers: UserPresence
   allUsersInRoom: Record<string, UserPresenceTrack & { isOnline: boolean }>
   channel: RealtimeChannel | null
   isConnected: boolean
@@ -57,4 +57,45 @@ export type UserPresenceStore = {
   updateCurrentSong: (song: Song, user: User) => void
   setOnlineUsers: (onlineUsers: UserPresence) => void
   disconnect: () => void
+}
+
+export type DMMessage = {
+  id: Tables<'messages'>['id']
+  user_sender_id: Tables<'messages'>['user_sender_id']
+  user_recipient_id: Tables<'messages'>['user_recipient_id']
+  content: Tables<'messages'>['content']
+  created_at: Tables<'messages'>['created_at']
+}
+
+export type DMBroadcastPayLoad = {
+  message: DMMessage
+  sender: Pick<Tables<'users'>, 'id' | 'full_name' | 'avatar_url'>
+  temp_id?: string
+}
+
+export type DMChatState = {
+  channel: RealtimeChannel
+  messages: DMMessage[]
+  isSubscribed: boolean
+  isTyping: boolean
+  lastMessageAt: string | null
+  hasLoadingHistory: boolean
+}
+
+export type TypingEvent = {
+  user_id: string
+  is_typing: boolean
+  chat_id: string
+}
+
+export type DMStore = {
+  activeChats: Record<Tables<'users'>['id'], DMChatState>
+  currentUserId: Tables<'users'>['id'] | null
+  openDM: (currentUserId: Tables<'users'>['id'], targetUserId: Tables<'users'>['id']) => Promise<void>
+  sendMessage: (targetUserId: Tables<'users'>['id'], message: Tables<'messages'>['content']) => Promise<void>
+  closeDM: (targetUserId: Tables<'users'>['id']) => Promise<void>
+  setTyping: (targetUserId: Tables<'users'>['id'], isTyping: boolean) => void
+  loadMessageHistory: (targetUserId: Tables<'users'>['id']) => Promise<void>
+  getChatWithUser: (targetUserId: Tables<'users'>['id']) => DMChatState | null
+  cleanup: () => void
 }
