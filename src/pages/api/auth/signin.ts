90522e3baffc362330fb9d32568c117f6dc56cf7
Crisops <crialeperez1835@gmail.com>
2025-08-15 17:38:23 -0500

    Actualizar la variable de entorno `URL_SITE` en el archivo `signin.ts` para utilizar `PUBLIC_SITE_URL` en lugar de `SITE_URL`, mejorando la configuración de la URL del sitio.

--- src/pages/api/auth/signin.ts
+++ src/pages/api/auth/signin.ts
@@ -5,7 +5,7 @@ import type { Provider } from '@supabase/supabase-js'
 export const POST: APIRoute = async ({ request, redirect, cookies }) => {
   const formData = await request.formData()
   const provider = formData.get('provider')?.toString()
-  const URL_SITE = import.meta.env.SITE_URL ?? 'http://localhost:4321'
+  const URL_SITE = import.meta.env.PUBLIC_SITE_URL ?? 'http://localhost:4321'
 
   const validProviders = ['google']
 
